f8a914e5737587f038fd4793473707e44278d6ab
Patrick Skjennum <mail@habitats.no>
2014-08-24 13:37:29 +0200

    added stuff

---
+++ src/main/java/aiprog/Main.java
@@ -0,0 +1,12 @@
+package aiprog;
+
+import a_star.Astar;
+
+/**
+ * Created by Patrick on 24.08.2014.
+ */
+public class Main {
+  public static void main(String args[]) {
+    new Astar().run();
+  }
+}
