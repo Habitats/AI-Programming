15204a6a824aeafe297be24de2810b16e711c935
Habitats <mail@habitats.no>
2014-10-10 23:38:44 +0200

    added new bullshit files

---
+++ src/main/java/algorithms/csp/canonical_utils/CanonicalConstraint.java
@@ -0,0 +1,11 @@
+package algorithms.csp.canonical_utils;
+
+/**
+ * Created by anon on 10.10.2014.
+ */
+public class CanonicalConstraint extends Constraint{
+
+  public CanonicalConstraint(VariableList variables, String expression) {
+    super(variables, expression);
+  }
+}
