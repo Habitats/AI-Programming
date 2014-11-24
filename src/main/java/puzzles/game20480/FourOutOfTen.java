e0b72b679baa3f724e2a43f38e75bfa309f04472
Patrick <mail@habitats.no>
2014-11-24 10:40:09 +0100

    formatting and comments

--- src/main/java/puzzles/game20480/FourOutOfTen.java
+++ src/main/java/puzzles/game20480/FourOutOfTen.java
@@ -23,21 +23,27 @@ public class FourOutOfTen {
 //    score += (Game2048Tile.VALUE.values().length - b.getUniqueValuesCount()) * 100;
     if (tileInCorner(max)) {
       score += 30000 * max.getValue().VAL;
+
       Game2048Tile second = sortedItems.get(1);
       if (second.x == 3 && second.y == 2) {
         score += 100 * sortedItems.get(1).getValue().VAL;
+
         Game2048Tile third = sortedItems.get(2);
         if (third.x == 3 && third.y == 1) {
           score += third.getValue().VAL;
+
           Game2048Tile forth = sortedItems.get(3);
           if (forth.x == 3 && forth.y == 0) {
             score += forth.getValue().VAL;
+
             Game2048Tile fifth = sortedItems.get(4);
             if (fifth.x == 2 && fifth.y == 0) {
               score += fifth.getValue().VAL;
+
               Game2048Tile sixth = sortedItems.get(5);
               if (sixth.x == 2 && sixth.y == 1) {
                 score += 10 * sixth.getValue().VAL;
+
                 Game2048Tile seventh = sortedItems.get(6);
                 if (seventh.x == 2 && seventh.y == 2) {
                   score += 10 * seventh.getValue().VAL;
@@ -60,6 +66,8 @@ public class FourOutOfTen {
     } else if (max.x == 2) {
       score -= 10;
     }
+
+    // penalize if low values to the right
     for (int y = 0; y < 3; y++) {
       if (b.get(3, y).getValue() == v0) {
         score -= 100 * sortedItems.get(0).getValue().VAL;
@@ -75,7 +83,11 @@ public class FourOutOfTen {
         }
       }
     }
+
+    // bonus for empty tiles
     score += b.getEmptyTiles().size() * 1000;
+
+    // exponential bonus for sum of the board
     for (Game2048Tile tile : b.getItems()) {
       score += Math.pow(tile.getValue().VAL, 2);
     }
